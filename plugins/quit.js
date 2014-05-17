4b1cb160c65174f96fdb68c82500c4ceb176a179
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2014-05-17 13:56:33 -0400

    adapt the quit plugin to the new framework and add an optional exit code

--- plugins/quit.js
+++ plugins/quit.js
@@ -1,6 +1,17 @@
-module.exports = function (nick, text, message, client) {
-  var match = text.match(/^\$quit/);
-  if (match) {
-    process.exit(0);
+module.exports = function (bot, core, config) {
+
+  var regex = /^\$quit\s?(\d+)?$/;
+
+  var listener = function (nick, text, msg) {
+    var match = text.match(regex);
+    if (match) {
+      process.exit(match[1] ? parseInt(match[1]) : 0);
+    }
   }
-};
+  bot.on('pub', listener);
+
+  // Return a destructor, will be called when plugin is unloaded (or reloaded)
+  return function () {
+    bot.removeListener('pub', listener);
+  };
+}
